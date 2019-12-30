1b15c4ca4f8d2aa7c1149cbbbe3a45287aae3c13
dudghk12@ <shhan6485@gmail.com>
2019-12-30 18:39:35 +0900

    12/30

---
+++ 4_WebFront/3_Javascript/3_Javascript/js/sample.js
@@ -0,0 +1,6 @@
+// 별도로 작성된 자바스크립트 샘플 
+
+function samp(){
+    window.alert('별도로 작성된 samp()함수 실행 확인');
+    Window.console.log('별도로 작성된 samp()함수 실행 확인');
+}
---
+++ 4_WebFront/3_Javascript/js/sample.js
@@ -0,0 +1,6 @@
+// 별도로 작성된 자바스크립트 샘플
+function sample (){
+    window.alert ('별도로 작성된 sample() 함수 실행 확인.');
+    window.console.log('별도로 작성된 sample() 함수 실행 확인.');
+}
+
